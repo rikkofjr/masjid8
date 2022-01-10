b70cd517d5040d7a66c0886bfca6a6f45722cb76
Rikko <fajarrico@rocketmail.com>
2022-01-10 18:25:30 +0700

    Update Layout Dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -75,7 +75,7 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design Template By <a href="https://nauval.in/">Stisla</a>
+          Copyright &copy; 2018 <div class="bullet"></div> Design Template By <a href="https://getstisla.com/">Stisla</a>
         </div>
         <div class="footer-right">
           2.3.0
