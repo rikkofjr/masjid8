8383c0dfd2feb7104b03fd8c9838eca5dd23cedf
Rikko <fajarrico@rocketmail.com>
2022-01-10 18:15:25 +0700

    Edit dashboard Layout blade

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -75,7 +75,7 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; 2018 <div class="bullet"></div> Design Template By <a href="https://nauval.in/">Stisla</a>
         </div>
         <div class="footer-right">
           2.3.0
